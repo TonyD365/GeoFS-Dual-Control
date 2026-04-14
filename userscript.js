4557f3c8bd1505550d7fb6f1cc8cc6c33667e4db
TonyD365 <tonydong365@outlook.com>
2026-04-14 11:59:27 -0400

    Bump version to 5.2.1 and update URLs

--- userscript.js
+++ userscript.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         GeoFS Dual Control Final
 // @namespace    geofs.dual.control.final
-// @version      5.2.0
+// @version      5.2.1
 // @description  Host/Copilot dual control for GeoFS on HF Space
 // @match        https://www.geofs.com/*
 // @match        http://www.geofs.com/*
 // @match        https://www.geo-fs.com/geofs.php?v=3.9
 // @grant        none
-// @downloadURL  https://raw.githubusercontent.com/TonyD365/GeoFS-Dual-Control/refs/heads/main/userscript.js
-// @updateURL    https://raw.githubusercontent.com/TonyD365/GeoFS-Dual-Control/refs/heads/main/userscript.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/TonyD365/GeoFS-Dual-Control@main/userscript.js
+// @updateURL    https://cdn.jsdelivr.net/gh/TonyD365/GeoFS-Dual-Control@main/userscript.js
 // ==/UserScript==
 
 (function () {
